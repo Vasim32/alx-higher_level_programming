6f86aa949e0e2dde28f96901478013a7eb4aa5bf
Vasim32 <Basimnuru@gmail.com>
2022-10-02 20:21:17 +0000

    edit files

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    userin = argv[1:]
+    size = len(userin)
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) is not 1 else "argument",
+                 "." if (size) is 0 else ":"))
+    for i in range (length1):
+        print("{:d}: {:s}".format(i + 1, userin[i]))
