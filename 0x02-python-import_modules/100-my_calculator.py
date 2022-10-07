3df87d127f0fd0831e10df877874abe572a2e120
Vasim32 <Basimnuru@gmail.com>
2022-10-07 04:47:13 +0000

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,33 @@
+#!/usr/bin/python3
+
 if __name__ == "__main__":
+    """a program that imports all functions from the
+    file calculator_1.py and handles basic operations."""
+
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+    args = sys.argv
+    operators = ['+', '-', '*', '/']
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+    if len(args) == 4:
+        a = int(args[1])
+        op = args[2]
+        b = int(args[3])
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        if op not in operators:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        else:
+            if op == '+':
+                print(f"{a} {op} {b} = {add(a, b)}")
+            elif op == '-':
+                print(f"{a} {op} {b} = {sub(a, b)}")
+            elif op == '*':
+                print(f"{a} {op} {b} = {mul(a, b)}")
+            else:
+                print(f"{a} {op} {b} = {div(a, b)}")
+            sys.exit(0)
+    else:
+        print(f"Usage: {args[0]} <a> <operator> <b>")
+        sys.exit(1)
